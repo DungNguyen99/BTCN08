fe3b2a589ce0f7dbb74376846461b0e6997497eb
DungNguyen99 <58107390+DungNguyen99@users.noreply.github.com>
2019-11-24 08:18:52 +0700

    Add files via upload

---
+++ init.php
@@ -0,0 +1,27 @@
+<?php
+// Gọi thư viện
+require_once ('./vendor/autoload.php');
+require_once 'config.php';
+require_once 'function.php';
+ob_start();
+
+//Thông báo lỗi
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Start session
+session_start();
+
+//Detect Page
+$page = detectPage();
+
+//Connect Database
+$db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8",$DB_USER, $DB_PASSWORD);
+
+//Detect User
+$currentUser = null;
+if (isset($_SESSION['userID']))
+{
+	$currentUser = findUserById($_SESSION['userID']);
+}
